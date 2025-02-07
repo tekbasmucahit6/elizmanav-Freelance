2dba74c5e3ce1dec22281578ecb9406a631eff69
mücahit tekbaş <tekbasmucahit6@gmail.com>
2025-02-07 18:26:34 +0300

    commit

--- src/pages/home/components/Products.tsx
+++ src/pages/home/components/Products.tsx
@@ -2,144 +2,111 @@ const products = [
   {
     id: 1,
     name: "Elma",
-    price: "3.50 TL",
-    img: "https://media.istockphoto.com/id/184276818/tr/fotoğraf/red-apple.jpg?s=2048x2048&w=is&k=20&c=v68BXBLrs-11QRX7XIjuDVI_ZOr1hJo-ztf3fYMXW1g=",
+    price: "₺20/kg",
+    image:
+      "https://media.istockphoto.com/id/184276818/tr/fotoğraf/red-apple.jpg?s=2048x2048&w=is&k=20&c=v68BXBLrs-11QRX7XIjuDVI_ZOr1hJo-ztf3fYMXW1g=",
   },
   {
     id: 2,
     name: "Muz",
-    price: "4.00 TL",
-    img: "https://media.istockphoto.com/id/1400057530/tr/fotoğraf/bananas-isolated.jpg?s=2048x2048&w=is&k=20&c=IlMZB6kdy-jHcB7OmPnpRV50i5btIwlRQjXo7sLr_iA=",
+    price: "₺30/kg",
+    image:
+      "https://media.istockphoto.com/id/1400057530/tr/fotoğraf/bananas-isolated.jpg?s=2048x2048&w=is&k=20&c=IlMZB6kdy-jHcB7OmPnpRV50i5btIwlRQjXo7sLr_iA=",
   },
   {
     id: 3,
     name: "Portakal",
-    price: "2.75 TL",
-    img: "https://media.istockphoto.com/id/185284489/tr/fotoğraf/orange.jpg?s=2048x2048&w=is&k=20&c=oXojQZeYZe75hp-eQVAnlM9uCtucpuNkwFFv-moxqCI=",
+    price: "₺25/kg",
+    image:
+      "https://media.istockphoto.com/id/185284489/tr/fotoğraf/orange.jpg?s=2048x2048&w=is&k=20&c=oXojQZeYZe75hp-eQVAnlM9uCtucpuNkwFFv-moxqCI=",
   },
   {
     id: 4,
-    name: "Domates",
-    price: "1.50 TL",
-    img: "https://media.istockphoto.com/id/1450576005/tr/fotoğraf/tomato-isolated-tomato-on-white-background-perfect-retouched-tomatoe-side-view-with.jpg?s=2048x2048&w=is&k=20&c=WHI_RWDAvSr1ly9FcUW-RzGCIY_88jyU02qf9INkBOA=",
+    name: "Çilek",
+    price: "₺50/kg",
+    image: "https://via.placeholder.com/150",
   },
   {
     id: 1,
     name: "Elma",
-    price: "3.50 TL",
-    img: "https://media.istockphoto.com/id/184276818/tr/fotoğraf/red-apple.jpg?s=2048x2048&w=is&k=20&c=v68BXBLrs-11QRX7XIjuDVI_ZOr1hJo-ztf3fYMXW1g=",
+    price: "₺20/kg",
+    image:
+      "https://media.istockphoto.com/id/184276818/tr/fotoğraf/red-apple.jpg?s=2048x2048&w=is&k=20&c=v68BXBLrs-11QRX7XIjuDVI_ZOr1hJo-ztf3fYMXW1g=",
   },
   {
     id: 2,
     name: "Muz",
-    price: "4.00 TL",
-    img: "https://media.istockphoto.com/id/1400057530/tr/fotoğraf/bananas-isolated.jpg?s=2048x2048&w=is&k=20&c=IlMZB6kdy-jHcB7OmPnpRV50i5btIwlRQjXo7sLr_iA=",
+    price: "₺30/kg",
+    image:
+      "https://media.istockphoto.com/id/1400057530/tr/fotoğraf/bananas-isolated.jpg?s=2048x2048&w=is&k=20&c=IlMZB6kdy-jHcB7OmPnpRV50i5btIwlRQjXo7sLr_iA=",
   },
   {
     id: 3,
     name: "Portakal",
-    price: "2.75 TL",
-    img: "https://media.istockphoto.com/id/185284489/tr/fotoğraf/orange.jpg?s=2048x2048&w=is&k=20&c=oXojQZeYZe75hp-eQVAnlM9uCtucpuNkwFFv-moxqCI=",
+    price: "₺25/kg",
+    image:
+      "https://media.istockphoto.com/id/185284489/tr/fotoğraf/orange.jpg?s=2048x2048&w=is&k=20&c=oXojQZeYZe75hp-eQVAnlM9uCtucpuNkwFFv-moxqCI=",
   },
   {
     id: 4,
-    name: "Domates",
-    price: "1.50 TL",
-    img: "https://media.istockphoto.com/id/1450576005/tr/fotoğraf/tomato-isolated-tomato-on-white-background-perfect-retouched-tomatoe-side-view-with.jpg?s=2048x2048&w=is&k=20&c=WHI_RWDAvSr1ly9FcUW-RzGCIY_88jyU02qf9INkBOA=",
+    name: "Çilek",
+    price: "₺50/kg",
+    image: "https://via.placeholder.com/150",
   },
   {
     id: 1,
     name: "Elma",
-    price: "3.50 TL",
-    img: "https://media.istockphoto.com/id/184276818/tr/fotoğraf/red-apple.jpg?s=2048x2048&w=is&k=20&c=v68BXBLrs-11QRX7XIjuDVI_ZOr1hJo-ztf3fYMXW1g=",
+    price: "₺20/kg",
+    image:
+      "https://media.istockphoto.com/id/184276818/tr/fotoğraf/red-apple.jpg?s=2048x2048&w=is&k=20&c=v68BXBLrs-11QRX7XIjuDVI_ZOr1hJo-ztf3fYMXW1g=",
   },
   {
     id: 2,
     name: "Muz",
-    price: "4.00 TL",
-    img: "https://media.istockphoto.com/id/1400057530/tr/fotoğraf/bananas-isolated.jpg?s=2048x2048&w=is&k=20&c=IlMZB6kdy-jHcB7OmPnpRV50i5btIwlRQjXo7sLr_iA=",
+    price: "₺30/kg",
+    image:
+      "https://media.istockphoto.com/id/1400057530/tr/fotoğraf/bananas-isolated.jpg?s=2048x2048&w=is&k=20&c=IlMZB6kdy-jHcB7OmPnpRV50i5btIwlRQjXo7sLr_iA=",
   },
   {
     id: 3,
     name: "Portakal",
-    price: "2.75 TL",
-    img: "https://media.istockphoto.com/id/185284489/tr/fotoğraf/orange.jpg?s=2048x2048&w=is&k=20&c=oXojQZeYZe75hp-eQVAnlM9uCtucpuNkwFFv-moxqCI=",
+    price: "₺25/kg",
+    image:
+      "https://media.istockphoto.com/id/185284489/tr/fotoğraf/orange.jpg?s=2048x2048&w=is&k=20&c=oXojQZeYZe75hp-eQVAnlM9uCtucpuNkwFFv-moxqCI=",
   },
   {
     id: 4,
-    name: "Domates",
-    price: "1.50 TL",
-    img: "https://media.istockphoto.com/id/1450576005/tr/fotoğraf/tomato-isolated-tomato-on-white-background-perfect-retouched-tomatoe-side-view-with.jpg?s=2048x2048&w=is&k=20&c=WHI_RWDAvSr1ly9FcUW-RzGCIY_88jyU02qf9INkBOA=",
+    name: "Çilek",
+    price: "₺50/kg",
+    image: "https://via.placeholder.com/150",
   },
-  {
-    id: 1,
-    name: "Elma",
-    price: "3.50 TL",
-    img: "https://media.istockphoto.com/id/184276818/tr/fotoğraf/red-apple.jpg?s=2048x2048&w=is&k=20&c=v68BXBLrs-11QRX7XIjuDVI_ZOr1hJo-ztf3fYMXW1g=",
-  },
-  {
-    id: 2,
-    name: "Muz",
-    price: "4.00 TL",
-    img: "https://media.istockphoto.com/id/1400057530/tr/fotoğraf/bananas-isolated.jpg?s=2048x2048&w=is&k=20&c=IlMZB6kdy-jHcB7OmPnpRV50i5btIwlRQjXo7sLr_iA=",
-  },
-  {
-    id: 3,
-    name: "Portakal",
-    price: "2.75 TL",
-    img: "https://media.istockphoto.com/id/185284489/tr/fotoğraf/orange.jpg?s=2048x2048&w=is&k=20&c=oXojQZeYZe75hp-eQVAnlM9uCtucpuNkwFFv-moxqCI=",
-  },
-  {
-    id: 4,
-    name: "Domates",
-    price: "1.50 TL",
-    img: "https://media.istockphoto.com/id/1450576005/tr/fotoğraf/tomato-isolated-tomato-on-white-background-perfect-retouched-tomatoe-side-view-with.jpg?s=2048x2048&w=is&k=20&c=WHI_RWDAvSr1ly9FcUW-RzGCIY_88jyU02qf9INkBOA=",
-  },
-  {
-    id: 1,
-    name: "Elma",
-    price: "3.50 TL",
-    img: "https://media.istockphoto.com/id/184276818/tr/fotoğraf/red-apple.jpg?s=2048x2048&w=is&k=20&c=v68BXBLrs-11QRX7XIjuDVI_ZOr1hJo-ztf3fYMXW1g=",
-  },
-  {
-    id: 2,
-    name: "Muz",
-    price: "4.00 TL",
-    img: "https://media.istockphoto.com/id/1400057530/tr/fotoğraf/bananas-isolated.jpg?s=2048x2048&w=is&k=20&c=IlMZB6kdy-jHcB7OmPnpRV50i5btIwlRQjXo7sLr_iA=",
-  },
-  {
-    id: 3,
-    name: "Portakal",
-    price: "2.75 TL",
-    img: "https://media.istockphoto.com/id/185284489/tr/fotoğraf/orange.jpg?s=2048x2048&w=is&k=20&c=oXojQZeYZe75hp-eQVAnlM9uCtucpuNkwFFv-moxqCI=",
-  },
-  {
-    id: 4,
-    name: "Domates",
-    price: "1.50 TL",
-    img: "https://media.istockphoto.com/id/1450576005/tr/fotoğraf/tomato-isolated-tomato-on-white-background-perfect-retouched-tomatoe-side-view-with.jpg?s=2048x2048&w=is&k=20&c=WHI_RWDAvSr1ly9FcUW-RzGCIY_88jyU02qf9INkBOA=",
-  },
-];
 
+];
 export default function Products() {
   return (
-    <div className="flex flex-col justify-center items-center w-full ">
-      <h1 className="text-4xl font-bold mb-4">Ürünlerimiz</h1>
-      <div className="grid grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-6 px-2">
-        {
-          products.map((dt, i) => (
-            <div className="flex flex-col justify-start items-center shadow w-full xl:w-56 lg:w-56 md:w-56 border  h-82" key={i}>
-              <div className="w-full">
-                <img src={dt.img} className="w-full" alt="" />
-              </div>
-              <div className="flex flex-col justify-end items-center w-full h-full">
-                <p>{dt.name}</p>
-                <p>{dt.price}</p>
-              </div>
-            </div>
-          ))
-        }
-      </div>
+    <div className="p-6 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {products.map((product) => (
+        <div
+          key={product.id}
+          className="bg-white rounded-2xl shadow-lg hover:scale-105 transition-all flex flex-col relative"
+        >
+          <img
+            src={product.image}
+            alt={product.name}
+            className="w-full object-contain rounded-t-2xl pt-3"
+          />
+          <div className="p-4 text-center flex flex-col justify-end h-full">
+            <h2 className="text-lg font-semibold text-gray-800">{product.name}</h2>
+            <p className="text-gray-600 font-medium mt-2">{product.price}</p>
+          </div>
+          <div className="absolute top-1 right-2">
+            <p className="bg-green-700 hover:bg-green-700 text-white py-2 px-2 rounded text-xs">
+              %100 doğal
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
+
