a59efb04e3fee237cf44055d27df1464953ede35
mücahit tekbaş <tekbasmucahit6@gmail.com>
2025-02-09 17:26:52 +0300

    commit

--- src/pages/home/components/HeroSection.tsx
+++ src/pages/home/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 export default function HeroSection() {
   return (
-    <div className="min-h-[70vh] text-white flex flex-col justify-start items-start pl-5 pt-16  gap-5 bg-[url(./logo/elizLogo.jpg)] bg-cover bg-center">
+    <div className="min-h-[70vh] text-white flex flex-col justify-start items-start pl-5 pt-16  gap-5 bg-[url(logo/elizLogo.jpg)] bg-cover bg-center">
     </div>
   );
 }
